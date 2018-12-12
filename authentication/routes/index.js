c521c629d0157e5d17800c9f116e481c8b77c7c1
EC2 Default User <ec2-user@ip-172-31-46-242.us-east-2.compute.internal>
2018-12-12 05:06:24 +0000

    finished get route

--- authentication/routes/index.js
+++ authentication/routes/index.js
@@ -4,61 +4,56 @@ var expressSession = require('express-session');
 
 var users = require('../controllers/users_controller');
 console.log("before / Route");
-router.get('/', function(req, res){
-    console.log("/ Route");
-//    console.log(req);
-    console.log(req.session);
-    if (req.session.user) {
-      console.log("/ Route if user");
-      res.render('index', {username: req.session.username,
-                           msg:req.session.msg,
-                           color:req.session.color});
-    } else {
-      console.log("/ Route else user");
-      req.session.msg = 'Access denied!';
-      res.redirect('/login');
-    }
-});
-
-router.get('/quotes', function(req, res) {
-    console.log("/quotes route");
-    if(req.session.user){
-        Comment.find(function(err, comments){
-    if(err){ return next(err); }
-    res.json(comments);
-  });
-    }
-})
-
-router.get('/user', function(req, res){
-    console.log("/user Route");
-    if (req.session.user) {
-      res.render('user', {msg:req.session.msg});
-    } else {
-      req.session.msg = 'Access denied!';
-      res.redirect('/login');
-    }
-});
-router.get('/signup', function(req, res){
-    console.log("/signup Route");
-    if(req.session.user){
-      res.redirect('/');
-    }
-    res.render('signup', {msg:req.session.msg});
-});
-router.get('/login',  function(req, res){
-    console.log("/login Route");
-    if(req.session.user){
-      res.redirect('/');
-    }
-    res.render('login', {msg:req.session.msg});
-});
-router.get('/logout', function(req, res){
-    console.log("/logout Route");
-    req.session.destroy(function(){
-      res.redirect('/login');
+router.get('/', function(req, res) {
+  console.log("/ Route");
+  //    console.log(req);
+  console.log(req.session);
+  if (req.session.user) {
+    console.log("/ Route if user");
+    res.render('index', {
+      username: req.session.username,
+      msg: req.session.msg,
+      color: req.session.color
     });
+  }
+  else {
+    console.log("/ Route else user");
+    req.session.msg = 'Access denied!';
+    res.redirect('/login');
+  }
+});
+
+
+router.get('/user', function(req, res) {
+  console.log("/user Route");
+  if (req.session.user) {
+    res.render('user', { msg: req.session.msg });
+  }
+  else {
+    req.session.msg = 'Access denied!';
+    res.redirect('/login');
+  }
+});
+router.get('/signup', function(req, res) {
+  console.log("/signup Route");
+  if (req.session.user) {
+    res.redirect('/');
+  }
+  res.render('signup', { msg: req.session.msg });
+});
+router.get('/login', function(req, res) {
+  console.log("/login Route");
+  if (req.session.user) {
+    res.redirect('/');
+  }
+  res.render('login', { msg: req.session.msg });
+});
+router.get('/logout', function(req, res) {
+  console.log("/logout Route");
+  req.session.destroy(function() {
+    res.redirect('/login');
   });
+});
 router.post('/signup', users.signup);
 router.post('/user/update', users.updateUser);
 router.post('/user/delete', users.deleteUser);
@@ -69,11 +64,21 @@ var mongoose = require('mongoose');
 var Quote = mongoose.model('Quote');
 router.post('/quotes', function(req, res, next) {
   var quote = new Quote(req.body);
-  quote.save(function(err, quote){
-    if(err){ return next(err); }
+  quote.save(function(err, quote) {
+    if (err) { return next(err); }
     res.json(quote);
   });
 });
+
+router.get('/quotes', function(req, res, next) {
+  console.log("/quotes route");
+  if (req.session.user) {
+    Quote.find(function(err, quotes) {
+      if (err) { return next(err); }
+      res.json(quotes);
+    });
+  }
+})
 
 
 module.exports = router;

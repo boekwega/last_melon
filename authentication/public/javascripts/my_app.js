2e4ecdb87a2286bd2a21ddf7814f60335983acdf
AbbyB <boekwega@gmail.com>
2018-12-12 05:30:56 +0000

    debug hooking everything together

--- authentication/public/javascripts/my_app.js
+++ authentication/public/javascripts/my_app.js
@@ -1,16 +1,23 @@
 angular.module('myApp', []).
-  controller('myController', ['$scope', '$http',
-                              function($scope, $http) {
+controller('myController', ['$scope', '$http',
+  function($scope, $http) {
     $http.get('/user/profile')
-        .success(function(data, status, headers, config) {
-      $scope.user = data;
-      $scope.error = "";
-    }).
+      .success(function(data, status, headers, config) {
+        $scope.user = data;
+        $scope.error = "";
+      }).
     error(function(data, status, headers, config) {
       $scope.user = {};
       $scope.error = data;
     });
-    
-    
-  }]);
-  
+    $scope.quotes = [];
+    $scope.getAll = function() {
+      return $http.get('/quotes').success(function(data) {
+        angular.copy(data, $scope.quotes);
+      });
+    };
+    $scope.getAll();
+
+
+  }
+]);
